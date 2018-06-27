49cc18df5b4fa103c3195e4628c08a6ddff4ce0e
Liby Lee <liby99@icloud.com>
2018-06-26 21:13:01 -0700

    Can Compile

---
+++ src/extern/Scene.h
@@ -0,0 +1,41 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include <vector>
+
+#include "Camera.h"
+#include "Object.h"
+#include "Light.h"
+
+namespace rin {
+    
+    class Scene {
+    protected:
+        
+        Camera activeCamera;
+        std::vector<Light *> lights;
+        std::vector<Object *> objects;
+        
+    public:
+        
+        Camera & getCamera();
+        void setCamera(Camera & cam);
+        
+        // Basic object list
+        void addObject(Object & obj);
+        Object & getObject(int i);
+        int numObjects();
+        
+        // Light list
+        void addLight(Light & lgt);
+        Light & getLight(int i);
+        int numLights();
+        
+        // Update draw
+        void update();
+        void draw();
+    private:
+    };
+}
+
+#endif
